7d8768867e3e97423e966b2842c168219e129891
Thommy21 <thomasparedes11.th@gmail.com>
2025-04-07 09:23:40 +0100

    Add files via upload

--- portfolio/js/weather/app.js
+++ portfolio/js/weather/app.js
@@ -1,1 +1,26 @@
-s
+if (document.readyState === "loading") {
+    // Loading hasn't finished yet
+        document.addEventListener("DOMContentLoaded", populateTableRows);
+    } else {
+        // DOMContentLoaded has already fired
+        populateTableRows();
+    }
+    function populateTableRows() {
+    let strTableRows = `<tr>
+        <td><span>Summary</span></td>
+        <td>Row 1, Cell 2</td>
+    </tr>
+    <tr>
+        <td><span>Temperature</span></td>
+        <td>Row 2, Cell 2</td>
+    </tr>
+    <tr>
+        <td><span>Humidity</span></td>
+        <td>Row 3, Cell 2</td>
+   </tr>
+   <tr>
+        <td><span>Pressure</span></td>
+        <td>Row 4, Cell 2</td>
+    </tr>`;
+    document.querySelector("#table-weather-dublin tbody").innerHTML = strTableRows;
+};
